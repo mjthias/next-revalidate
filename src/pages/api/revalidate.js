ab200bae41a94b6a50175f2fb9d037794487b966
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-02 10:29:24 +0200

    error logged

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
     await res.revalidate(path);
     return res.json({ revalidated: true });
   } catch (err) {
+    console.error(err);
     return res.status(500).send("Error revalidating");
   }
 }
