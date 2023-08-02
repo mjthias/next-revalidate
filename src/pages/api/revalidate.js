aafcc87d43a2c6a36033c40f9618d109df272a99
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-02 09:35:08 +0200

    test 1

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  // Validate secret
+  if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  // Validate path
+  const path = req.query.path;
+  if (!path) return res.status(400).json({ message: "Please provide a path" });
+
+  try {
+    await res.revalidate(path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
